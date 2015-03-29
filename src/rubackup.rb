8831ae0a3cabb662e8e8198e4ae1f3a9800dff44
fdupoux <fdupoux@users.sourceforge.net>
2015-03-29 10:26:12 +0100

    Changed version number from "0.1.0" to "0.1.1git" to start working on next version

--- src/rubackup.rb
+++ src/rubackup.rb
@@ -26,7 +26,7 @@ require 'date'
 require 'yaml'
 
 # Define constants
-$RUBACKUP_VERSION = "0.1.0"
+$RUBACKUP_VERSION = "0.1.1git"
 $RUBACKUP_MIN_RUBY = 1.9 # Required by Open3.pipeline()
 
 # Define global variables
